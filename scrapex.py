d36c0013f3fe463f7af65491429f44824b19f6f1
Kyle Joseph Nadeau <kylejnadeau@gmail.com>
2024-05-22 15:57:46 -0400

    put info in df

--- scrapex.py
+++ scrapex.py
@@ -4,8 +4,16 @@ from ntscraper import Nitter
 
 scraper = Nitter()
 
-tweets = scraper.get_tweets('#Drake', mode = 'hashtag', number = 5)
-print(tweets)
+tweets = scraper.get_tweets('Drake', mode = 'hashtag', number = 5)
+
+final_tweets = []
+for tweet in tweets['tweets']:
+    data = [tweet['link'], tweet['text'], tweet['date']]
+    final_tweets.append(data)
+
+df = pd.DataFrame(final_tweets, columns =['link', 'text', 'date'])
+
+
 
 
 
