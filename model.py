73b3c38b1a2da0ecaa09acc9bb820256a2b864e6
Kyle Joseph Nadeau <kylejnadeau@gmail.com>
2024-05-24 01:13:10 -0400

    implementation of tokenization and verification

--- model.py
+++ model.py
@@ -8,17 +8,6 @@ import re
 import emoji
 
 
-
-#Load model and tokenizer
-#Transfer learning
-#Model is trained on examples pulled from Twitter
-def model(data):
-    tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-    model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-    model.eval()
-
-
-
 def preprocess(text):
     new_text = re.sub(r'https?://\S+|www\.\S+', '', text)
 
@@ -29,19 +18,39 @@ def preprocess(text):
     return new_text
 
 
-def processData():
+def processData(model, tokenizer):
     df = pd.read_csv("redditData.csv")
     df['features'] = df['1'] + ' ' + df['2']
     df['features'] = df['features'].astype(str)
 
     df['features'] = df['features'].apply(preprocess)
-    return df['features']
     
+    results = []
+    for text in df['features']:
+        #tokenize data for ML model
+        encoded_input = tokenizer(text, return_tensors='pt', truncation=True, max_length=512)
+        with torch.no_grad():
+            output = model(**encoded_input)
+            scores = output.logits.detach().numpy()[0]
+            probabilities = softmax(scores)
+            sentiment = np.argmax(probabilities)
+            results.append({'text': text, 'sentiment': sentiment, 'probability': probabilities[sentiment]})
+
+    results_df = pd.DataFrame(results)
+    return results_df
 
 
 
 if __name__ == "__main__":
-    #model()
-    data = processData()
-    print(data)
+    #Load model and tokenizer
+    #Transfer learning
+    #Model is trained on examples pulled from Twitter
+    tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+    model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+    model.eval()
+
+    results = processData(model, tokenizer)
+
+    results.to_csv("results.csv", index = False)
+    
     
